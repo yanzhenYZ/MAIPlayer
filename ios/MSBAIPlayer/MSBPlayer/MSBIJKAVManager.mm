713a8803a337b08f001c10e3e8e48d50793229f2
yanzhen <yanzhen@meishubao.com>
2020-09-02 11:54:59 +0800

    data delete

--- ios/MSBAIPlayer/MSBPlayer/MSBIJKAVManager.mm
+++ ios/MSBAIPlayer/MSBPlayer/MSBIJKAVManager.mm
@@ -7,6 +7,7 @@
 //
 
 #import "MSBIJKAVManager.h"
+#import "MSBAVMedia.h"
 
 #define AUDIO 0
 
@@ -61,55 +62,15 @@ static id _managet;
 }
 
 - (void)displayPixelBuffer:(CVPixelBufferRef)pixelBuffer {
-    if (_videoDataBlock) {
+    if (pixelBuffer && _videoDataBlock) {
         _videoDataBlock(pixelBuffer);
     }
 }
 
 - (void)yuv420PToPixelBuffer:(uint8_t *)yBuffer vBuffer:(uint8_t *)uBuffer uBuffer:(uint8_t *)vBuffer width:(int)width height:(int)height dataWidth:(int)dataWidth {
-    [self pixelBufferFromYUV:yBuffer vBuffer:uBuffer uBuffer:vBuffer width:width height:height dataWidth:dataWidth];
-}
-
--(void)pixelBufferFromYUV:(uint8_t *)yBuffer vBuffer:(uint8_t *)uBuffer uBuffer:(uint8_t *)vBuffer width:(int)width height:(int)height dataWidth:(int)dataWidth {
-    NSDictionary *pixelAttributes = @{(NSString *)kCVPixelBufferIOSurfacePropertiesKey:@{}};
-    
-    CVPixelBufferRef pixelBuffer = NULL;
-    CVReturn result = CVPixelBufferCreate(kCFAllocatorDefault,
-                                            width,
-                                            height,
-                                            kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange,
-                                            (__bridge CFDictionaryRef)(pixelAttributes),
-                                            &pixelBuffer);
-    if (result != kCVReturnSuccess) {
-        NSLog(@"MSB 002 Unable to create cvpixelbuffer %d", result);
-        return;
-    }
-    
-    CVPixelBufferLockBaseAddress(pixelBuffer, 0);
-    size_t dstStrideY  = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer, 0);
-    size_t dstStrideUV = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer, 1);
-    //size_t dstHY = CVPixelBufferGetHeightOfPlane(pixelBuffer, 0);
-    //size_t dstHUV = CVPixelBufferGetHeightOfPlane(pixelBuffer, 1);
-    unsigned char *yDestPlane = (unsigned char *)CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, 0);
-    size_t realY = dataWidth;
-    if (dstStrideY < realY) {
-        realY = dstStrideY;
-    }
-    for (int i = 0; i < height; i++) {
-        memcpy(yDestPlane + dstStrideY * i + 0, yBuffer + realY * i, realY);
-    }
-    
-    unsigned char *uvDestPlane = (unsigned char *)CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, 1);
-    for (int i = 0; i < height / 2; i++) {
-        for (int j = 0; j < width / 2; j++) {
-            unsigned long uvIndex = dstStrideUV * i + j * 2;
-            uvDestPlane[uvIndex] = uBuffer[realY / 2 * i + j];
-            uvDestPlane[uvIndex + 1] = vBuffer[realY / 2 * i + j];
-        }
-    }
-    
-    CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
+    CVPixelBufferRef pixelBuffer = [MSBAVMedia yuv420PToPixelBuffer:yBuffer uBuffer:uBuffer vBuffer:vBuffer width:width height:height dataWidth:dataWidth];
     [self displayPixelBuffer:pixelBuffer];
     CVPixelBufferRelease(pixelBuffer);
 }
+
 @end
